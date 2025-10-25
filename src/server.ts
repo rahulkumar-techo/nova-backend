eef824cbd149e9d68ce73960c4a58d006c7a8c32
Rahul kumar <rahulkumar7250883289@gmail.com>
2025-10-25 18:23:17 +0530

    connect with mongodb

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,21 @@
 import { log } from "@utils/logger";
 import app from "app";
 import configs from "configs";
+import { db_connection } from "configs/db_connection";
 
 
 
 const PORT = configs.port;
 
-app.listen(PORT, () => {
-  log.info(`Server started on port ${PORT} in ${configs.env} mode`);
-});
+
+
+(async () => {
+  try {
+    await db_connection();
+    app.listen(PORT, () => {
+      log.info(`Server started on port ${PORT} in ${configs.env} mode`);
+    });
+  } catch (error: any) {
+    log.error(`Failed to start server:\n ${error.message}`);
+  }
+})();
--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,9 +1,25 @@
 /**
- * Simple logger utility.
- * Replace with winston/pino for production needs.
+ * Winston console-only logger
+ * No files, only logs to console with colors and timestamps
  */
 
-export const log = {
-  info: (msg: string) => console.log(`[INFO] ${new Date().toISOString()} - ${msg}`),
-  error: (msg: string) => console.error(`[ERR] ${new Date().toISOString()} - ${msg}`)
-};
+import { createLogger, format, transports } from "winston";
+
+const { combine, timestamp, printf, colorize } = format;
+
+const logFormat = printf(({ level, message, timestamp }) => {
+  return `[${level.toUpperCase()}] ${timestamp} - ${message}`;
+});
+
+export const log = createLogger({
+  level: "info",
+  format: combine(
+    timestamp(),
+    colorize({ all: true }),
+    logFormat
+  ),
+  transports: [
+    new transports.Console()
+  ],
+  exitOnError: false
+});
