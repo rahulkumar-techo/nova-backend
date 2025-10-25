7fad7e375b492a33ecb4ced11c575e729bf447cc
Rahul kumar <rahulkumar7250883289@gmail.com>
2025-10-25 18:32:40 +0530

    connected with local mongo_db

---
+++ src/configs/db_connection.ts
@@ -0,0 +1,14 @@
+
+import mongoose from 'mongoose';
+
+const mongoURI = process.env.MONGO_URI || 'mongodb://localhost:27017/novadb';
+
+export const db_connection = async () => {
+    try {
+        await mongoose.connect(mongoURI);
+        console.log('Connected to MongoDB successfully');
+    } catch (error) {
+        console.error('Error connecting to MongoDB:', error);
+        process.exit(1);
+    }
+};
